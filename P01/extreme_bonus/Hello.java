169d307f9dbfb880e8ef1ce55c4c587114555252
Vikash Mall <vikashmall4242@gmail.com>
2024-08-21 18:18:30 -0500

    adding all files

---
+++ P01/extreme_bonus/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        String name = System.getProperty("user.name");
+        System.out.println("hello, "+ name +"!");
+    }
+}
