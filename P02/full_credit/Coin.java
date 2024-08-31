2b091c322f2d081ceca8cf07e999230bc20aef47
Vikash Mall <vikashmall4242@gmail.com>
2024-08-31 18:56:26 -0500

    Wrote down things in Coin class from UML

--- P02/full_credit/Coin.java
+++ P02/full_credit/Coin.java
@@ -1,3 +1,11 @@
 public class Coin {
-    
+    private Denomination denomination;
+    private int year;
+
+    public Coin(Denomination denomination, int year) {}
+
+    public double getValue() {}
+
+    public int getYear() {}
+
 }
