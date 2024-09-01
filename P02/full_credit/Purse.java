3181e03292f522241afa9c26b046a6eae249ddea
Vikash Mall <vikashmall4242@gmail.com>
2024-08-31 19:53:22 -0500

    Written print statements

--- P02/full_credit/Purse.java
+++ P02/full_credit/Purse.java
@@ -1,26 +1,29 @@
 public class Purse {
     public static void main(String[] args) {
         Coin[] purse = new Coin[] {
-            new Coin(Denomination.PENNY, 1850),
-            new Coin(Denomination.NICKEL, 1862),
-            new Coin(Denomination.DIME, 1905),
-            new Coin(Denomination.QUARTER, 1803),
-            new Coin(Denomination.NICKEL, 1915),
+                new Coin(Denomination.PENNY, 1850),
+                new Coin(Denomination.NICKEL, 1862),
+                new Coin(Denomination.DIME, 1905),
+                new Coin(Denomination.QUARTER, 1803),
+                new Coin(Denomination.NICKEL, 1915),
         };
 
         double sumCoins = 0.0;
         int earliestYear = Integer.MAX_VALUE;
         int latestYear = Integer.MIN_VALUE;
 
-        for(Coin coin : purse) {
+        for (Coin coin : purse) {
             sumCoins += coin.getValue();
 
-            if(coin.getYear() < earliestYear) {
+            if (coin.getYear() < earliestYear) {
                 earliestYear = coin.getYear();
-            } 
-            if(coin.getYear() > latestYear) {
+            }
+            if (coin.getYear() > latestYear) {
                 latestYear = coin.getYear();
             }
         }
+        System.out.println("Total value of coins in the purse: $" + sumCoins);
+        System.out.println("Earliest Year: " + earliestYear);
+        System.out.print("Latest Year: " + latestYear);
     }
 }
