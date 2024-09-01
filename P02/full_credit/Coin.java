c5480708f78c1390387a01cce01d7b375fc7acc5
Vikash Mall <vikashmall4242@gmail.com>
2024-08-31 19:17:08 -0500

    Wrote getValue function in class Coin  and also main function in class Purse

--- P02/full_credit/Coin.java
+++ P02/full_credit/Coin.java
@@ -2,9 +2,19 @@ public class Coin {
     private Denomination denomination;
     private int year;
 
-    public Coin(Denomination denomination, int year) {}
+    public Coin(Denomination denomination, int year) {
+        this.denomination = denomination;
+        this.year = year;
+    }
 
-    public double getValue() {}
+    public double getValue() {
+        return switch(denomination) {
+            case PENNY -> 0.01;
+            case NICKEL -> 0.05;
+            case DIME -> 0.10;
+            case QUARTER -> 0.25;
+        };
+    }
 
     public int getYear() {}
 
--- P02/full_credit/Purse.java
+++ P02/full_credit/Purse.java
@@ -1,3 +1,5 @@
 public class Purse {
-
+    public static void main(String[] args) {
+        
+    }
 }
