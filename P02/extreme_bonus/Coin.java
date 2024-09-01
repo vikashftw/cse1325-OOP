47b5aa2a75c0c9ee9f42fbfae0eaa9b2d4f6c4c8
Vikash Mall <vikashmall4242@gmail.com>
2024-08-31 21:19:59 -0500

    Copied code from bonus to extreme_bonus

--- P02/extreme_bonus/Coin.java
+++ P02/extreme_bonus/Coin.java
@@ -1,3 +1,22 @@
 public class Coin {
-    
+    private Denomination denomination;
+    private int year;
+
+    public Coin(Denomination denomination, int year) {
+        this.denomination = denomination;
+        this.year = year;
+    }
+
+    public double getValue() {
+        return denomination.getValue();
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return year + " " + denomination.toString();
+    }
 }
--- P02/extreme_bonus/Denomination.java
+++ P02/extreme_bonus/Denomination.java
@@ -1,3 +1,18 @@
-public class Denomination {
-    
+public enum Denomination {
+    PENNY(0.01), NICKEL(0.05), DIME(0.10), QUARTER(0.25);
+
+    private double value;
+
+    private Denomination(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return name().toLowerCase();
+    }
 }
