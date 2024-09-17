965fb446db9d1dd3ef9677fbc8cb49ed34ccf4bf
Vikash Mall <vikashmall4242@gmail.com>
2024-09-16 22:01:18 -0500

    Removed unnecessary code and created Alacarte

--- P04/customer/Unlimited.java
+++ P04/customer/Unlimited.java
@@ -3,8 +3,4 @@ package customer;
 import product.Media;
 
 public class Unlimited extends Account {
-    @Override
-    public String play(Media media) {
-        return "Playing "+ media.toString();
-    }
 }
