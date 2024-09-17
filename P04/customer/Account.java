14d68ec0b1136796bd0ac1d0b286c1c08fd12fd6
Vikash Mall <vikashmall4242@gmail.com>
2024-09-16 22:29:41 -0500

    Modified Unlimited and Account class

--- P04/customer/Account.java
+++ P04/customer/Account.java
@@ -2,7 +2,7 @@ package customer;
 
 import product.Media;
 
-public class Account {
+public abstract class Account {
     
     private int accountNumber;
     private static int nextAccountNumber = 1;
@@ -13,7 +13,5 @@ public class Account {
     public int getAccountNumber() {
         return accountNumber;
     }
-    public String play(Media media) {
-        return "Playing "+ media.toString();
-    }
+    public abstract String play(Media media);
 }
--- P04/customer/Unlimited.java
+++ P04/customer/Unlimited.java
@@ -3,4 +3,8 @@ package customer;
 import product.Media;
 
 public class Unlimited extends Account {
+    @Override
+    public String play(Media media) {
+        return "Playing "+ media.toString();
+    }
 }
