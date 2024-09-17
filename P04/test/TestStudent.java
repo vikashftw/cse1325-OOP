109604dde61a28a29525dce3388f95d09b73fb46
Vikash Mall <vikashmall4242@gmail.com>
2024-09-17 02:00:06 -0500

    Final commit for P04, updated TestStudent class

--- P04/test/TestStudent.java
+++ P04/test/TestStudent.java
@@ -1,6 +1,7 @@
 package test;
 
 import customer.Student;
+import customer.Alacarte;
 import product.Media;
 
 public class TestStudent {
@@ -28,13 +29,44 @@ public class TestStudent {
             }
         }
 
-        
         Media media = new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0", points);
         String playResult = student.requestMedia(media);
-        String expectedPlayResult = "Playing The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0), "+ points + " points";
+        String expectedPlayResult = "Playing The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0), " + points + " points";
 
         if (!expectedPlayResult.equals(playResult)) {
             System.out.println("FAIL: Student requestMedia() returned incorrect value.");
+            System.exit(1);
+        }
+
+        // New Tests Below for Alacarte and Unlimited account:
+
+        Media unlimitedMedia = new Media("Unlimited Media", "https://example.com/unlimitedmedia", 5);
+        String unlimitedPlayResult = student.requestMedia(unlimitedMedia);
+        String expectedUnlimitedResult = "Playing Unlimited Media (https://example.com/unlimitedmedia), 5 points";
+        if (!unlimitedPlayResult.equals(expectedUnlimitedResult)) {
+            System.out.println("FAIL: Unlimited account's requestMedia() did not return the expected value.");
+            System.exit(1);
+        }
+
+        Student alacarteStudent = new Student("John Doe", 1007654321, "john.doe@mavs.uta.edu", false);
+        
+        ((Alacarte) alacarteStudent.getAccount()).buyPoints(10);
+        
+        Media alacarteMedia = new Media("Test Media with 5 points", "https://example.com/testmedia", 5);
+        String alacartePlayResult = alacarteStudent.requestMedia(alacarteMedia);
+        String expectedAlacarteResult = "Playing Test Media with 5 points (https://example.com/testmedia), 5 points";
+        
+        if (!alacartePlayResult.equals(expectedAlacarteResult)) {
+            System.out.println("FAIL: Alacarte account's requestMedia() did not return correct result when sufficient points were available.");
+            System.exit(1);
+        }
+
+        Media expensiveMedia = new Media("Expensive Media", "https://example.com/expensivemedia", 10); // Media requires 10 points
+        String alacarteInsufficientResult = alacarteStudent.requestMedia(expensiveMedia);
+        String expectedInsufficientResult = "Buy more points: Requires 10 points, you have 5";
+        
+        if (!alacarteInsufficientResult.equals(expectedInsufficientResult)) {
+            System.out.println("FAIL: Alacarte account did not return correct result when insufficient points were available.");
             System.exit(1);
         }
 
