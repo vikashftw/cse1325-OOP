c397ade6d33a75ac553146dde34503fc59d413dc
Vikash Mall <vikashmall4242@gmail.com>
2024-09-16 22:17:26 -0500

    Implemented Alacarte class

--- P04/customer/Alacarte.java
+++ P04/customer/Alacarte.java
@@ -1,5 +1,27 @@
 package customer;
 
+import product.Media;
+
 public class Alacarte extends Account {
-    
+    private int pointsRemaining;
+
+    public void buyPoints(int points) {
+        pointsRemaining += points;
+    }
+
+    public int getPointsRemaining() {
+        return pointsRemaining;
+    }
+
+    @Override
+    public String play(Media media) {
+        int mediaPoints = media.getPoints();
+
+        if(pointsRemaining >= mediaPoints) {
+            pointsRemaining -= mediaPoints;
+            return "Playing " + media.toString();
+        } else {
+            return "Buy more points: Requires " + mediaPoints + " points, you have " + pointsRemaining + " points.";
+        }
+    }
 }
