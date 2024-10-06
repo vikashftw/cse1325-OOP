6723dfbed8d2ec03a58e5943c41dc48847bcaf4f
Vikash Mall <vikashmall4242@gmail.com>
2024-10-06 18:29:05 -0500

    Update Alacarte.java

--- P06/customer/Alacarte.java
+++ P06/customer/Alacarte.java
@@ -1,5 +1,9 @@
 package customer;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 import product.Media;
 
 /**
@@ -12,6 +16,42 @@ import product.Media;
  */
 public class Alacarte extends Account {
     private int pointsRemaining = 0;
+
+    /**
+     * Constructs an Alacarte account with a unique account number and initial points.
+     * Calls the superclass constructor to initialize the account.
+     * 
+     * @since 1.0
+     */
+    public Alacarte() {
+        super();
+    }
+
+    /**
+     * Constructs an Alacarte account by reading values from a BufferedReader.
+     * Calls the superclass constructor to initialize the account fields and reads pointsRemaining.
+     *
+     * @param br BufferedReader object for reading the account fields.
+     * @throws IOException if an I/O error occurs
+     * @since 1.0
+     */
+    public Alacarte(BufferedReader br) throws IOException {
+        super(br);
+        this.pointsRemaining = Integer.parseInt(br.readLine());
+    }
+
+    /**
+     * Saves the Alacarte account fields to a file using the provided BufferedWriter.
+     * Calls the superclass save method to handle the base fields and then writes pointsRemaining.
+     *
+     * @param bw BufferedWriter object for writing the account fields.
+     * @throws IOException if an I/O error occurs
+     * @since 1.0
+     */
+    public void save(BufferedWriter bw) throws IOException {
+        super.save(bw);
+        bw.write(Integer.toString(pointsRemaining) + "\n");
+    }
 
     /**
      * Adds points to the Alacarte account.
