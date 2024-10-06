01fe1a1dc4fadf3e615a0459dbda8681e6843ac6
Vikash Mall <vikashmall4242@gmail.com>
2024-10-06 18:22:26 -0500

    Update Unlimited.java

--- P06/customer/Unlimited.java
+++ P06/customer/Unlimited.java
@@ -1,5 +1,9 @@
 package customer;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 import product.Media;
 
 /**
@@ -10,6 +14,40 @@ import product.Media;
  * @since 1.0
  */
 public class Unlimited extends Account {
+
+    /**
+     * Constructs an Unlimited account with a unique account number.
+     * Calls the superclass constructor to initialize the account.
+     * 
+     * @since 1.0
+     */
+    public Unlimited() {
+        super();
+    }
+
+    /**
+     * Constructs an Unlimited account by reading values from a BufferedReader.
+     * Calls the superclass constructor to initialize the account fields.
+     *
+     * @param br BufferedReader object for reading the account fields.
+     * @throws IOException if an I/O error occurs
+     * @since 1.0
+     */
+    public Unlimited(BufferedReader br) throws IOException {
+        super(br);
+    }
+
+    /**
+     * Saves the Unlimited account fields to a file using the provided BufferedWriter.
+     * Calls the superclass save method to handle the base fields.
+     *
+     * @param bw BufferedWriter object for writing the account fields.
+     * @throws IOException if an I/O error occurs
+     * @since 1.0
+     */
+    public void save(BufferedWriter bw) throws IOException {
+        super.save(bw);
+    }
 
     /**
      * Plays the media for an Unlimited account without deducting any points.
