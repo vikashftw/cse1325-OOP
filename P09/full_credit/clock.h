7a882e91ef8c5786fb0aaf49b2e3e43c11b45090
Vikash Mall <vikashmall4242@gmail.com>
2024-11-03 19:49:46 -0600

    Write clock.h from UML

---
+++ P09/full_credit/clock.h
@@ -0,0 +1,22 @@
+#ifndef CLOCK_H
+#define CLOCK_H
+
+#include <iostream>
+#include <iomanip>
+#include <stdexcept>
+#include <string>
+
+class Clock {
+protected:
+    int _hours;
+    int _minutes;
+    int _seconds;
+
+public:
+    Clock(int hours, int minutes, int seconds);
+    virtual ~Clock() {}
+    void tic();
+    void print() const;
+};
+
+#endif
