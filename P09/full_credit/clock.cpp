166aa0efe9552a8f5983ea4f98b3a976bc8c7c3a
Vikash Mall <vikashmall4242@gmail.com>
2024-11-03 19:50:50 -0600

    Add tic and print function

--- P09/full_credit/clock.cpp
+++ P09/full_credit/clock.cpp
@@ -11,3 +11,21 @@ Clock::Clock(int hours, int minutes, int seconds) : _hours(hours), _minutes(minu
         throw std::out_of_range("Second out of range: " + std::to_string(_seconds));
     }
 }
+
+void Clock::tic() {
+    _seconds++;
+    if (_seconds == 60) {
+        _seconds = 0;
+        _minutes++;
+        if (_minutes == 60) {
+            _minutes = 0;
+            _hours = (_hours + 1) % 24;
+        }
+    }
+}
+
+void Clock::print() const {
+    std::cout << std::setw(2) << std::setfill('0') << _hours << ":"
+              << std::setw(2) << std::setfill('0') << _minutes << ":"
+              << std::setw(2) << std::setfill('0') << _seconds;
+}
