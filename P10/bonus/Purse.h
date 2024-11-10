ac5c4d378b0b075e6d749b2e8d42f8c994786eaa
Vikash Mall <vikashmall4242@gmail.com>
2024-11-10 03:15:25 -0600

    Copy all code from full credit

---
+++ P10/bonus/Purse.h
@@ -0,0 +1,24 @@
+#ifndef __PURSE_H
+#define __PURSE_H
+#include <iostream>
+
+class Purse {
+    private:
+        int _pounds;
+        int _shillings;
+        int _pence;
+        void rationalize();
+    
+    public:
+        Purse(int pounds = 0, int shillings = 0, int pence = 0);
+        friend std::ostream& operator<<(std::ostream& ost, const Purse& purse);
+        auto operator<=>(const Purse& purse) const = default;
+        Purse& operator++();    
+        Purse operator++(int);  
+        Purse operator+(const Purse& purse) const;
+        Purse operator-(const Purse& purse) const;
+        Purse& operator+=(const Purse& purse);
+        Purse& operator-=(const Purse& purse);
+};
+
+#endif
