8f0590ab23b8e7c8ac6952dfb4cb09cbf85b09f6
Vikash Mall <vikashmall4242@gmail.com>
2024-11-18 19:26:10 -0600

    Write date.h from UML

---
+++ P11/full_credit/date.h
@@ -0,0 +1,17 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <ostream>
+
+class Date {
+    private:
+        int _year;
+        int _month;
+        int _day;
+        
+    public:
+        Date(int year = 1970, int month = 1, int day = 1);
+        auto operator<=>(const Date& date) const = default;
+        friend std::ostream& operator<<(std::ostream& ost, const Date& date);
+};
+#endif
